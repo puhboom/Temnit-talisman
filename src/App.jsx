866487aee88fdb0c914d5a9fd568df526ffd74fd
puhboom <55370896+puhboom@users.noreply.github.com>
2023-06-01 20:23:16 +0000

    initial page cleanup, on to functionality!

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import './App.css'
 import React from 'react';
+import './App.css';
 
 const App = () => {
   const handleCreate = (platform) => {
@@ -13,23 +13,23 @@ const App = () => {
   };
 
   return (
-    <div>
+    <main>
       <h1>Talisman, <i>know your audience</i></h1>
 
-      <button onClick={() => handleCreate('LinkedIn')}>Create with LinkedIn</button>
-      <button onClick={() => handleCreate('Facebook')}>Create with Facebook</button>
-      <button onClick={() => handleCreate('GitHub')}>Create with GitHub</button>
-      <button onClick={() => handleCreate('Instagram')}>Create with Instagram</button>
-      <button onClick={() => handleCreate('TikTok')}>Create with TikTok</button>
-      <button onClick={handleSAML}>Whomever does SAML</button>
-
-      <h3>What is Talisman?</h3>
-      <p>
-        Who is your biggest fan and critic? Link an account and Talisman reviews the past month of interactions.</p>
-      <p>What are people saying? Who is annoying? Who needs reporting?
+      <h2>What is talisman?</h2>
+      <p>Who is your biggest fan? ...and critic? Link an account and Talisman will review the past month of interactions.</p>
+      <p><li>What are people saying?</li><li> Who is annoying?</li>  <li>Who needs reporting?</li> </p>
+      <p><br></br><i><b>You are in charge.</b></i>
       </p>
-      <p>You are in charge.</p>
-    </div>
+
+      <button className="button primary-button" onClick={() => handleCreate('LinkedIn')}>Create with LinkedIn</button><br></br>
+      <button className="button primary-button" onClick={() => handleCreate('Facebook')}>Create with Facebook</button><br></br>
+      <button className="button primary-button" onClick={() => handleCreate('GitHub')}>Create with GitHub</button><br></br>
+      <button className="button primary-button" onClick={() => handleCreate('Instagram')}>Create with Instagram</button><br></br>
+      <button className="button primary-button" onClick={() => handleCreate('TikTok')}>Create with TikTok</button><br></br>
+      <button className="button primary-button" onClick={handleSAML}>Whomever does SAML</button>
+
+    </main>
   );
 };
 
